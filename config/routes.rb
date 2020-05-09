24894ff2c5e08f525faf9e07c2bdff2e82ddb7a2
vagrant <vagrant@localhost.localdomain>
2020-05-09 14:14:55 +0000

    [Update]mypage作成中！

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  get 'users/show'
+
+  resources :users, only: [:show]
+
+  devise_for :users, controllers: {
+  	       registrations: 'users/registrations'
+          }
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'books#index'
+
 end
