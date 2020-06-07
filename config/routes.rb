f3fa430977d0ff20c77230ae1b48c9e00b8c863d
vagrant <vagrant@localhost.localdomain>
2020-06-07 07:23:47 +0000

    [Update]フォロー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,13 @@ Rails.application.routes.draw do
 
    get "home/about" => "home#about",as: 'about'
 
-   resources :users, only: [:show, :edit, :index, :update]
+   resources :users, only: [:show, :edit, :index, :update] do
+   	member do
+   		get :following, :followers
+   	end
+   end
+   resources :relationships, only: [:create, :destroy]
+
    resources :books, only: [:create, :index, :show, :edit, :destroy, :update] do
    	 resources :book_comments, only: [:create,:destroy]
      resource :favorites, only: [:create,:destroy]
